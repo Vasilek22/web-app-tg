58f370b22b2dc59c39779e010ebcdeb65d036530
Vasilek22 <misavasin228@gmail.com>
2025-03-26 16:07:51 +0300

    Ваше сообщение

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Content from './components/Content/Content';
 import Navbar from './components/Navbar/Navbar';
 import { useTelegram } from './hooks/useTelegram';
+import Header from './components/Header/Header';
 
 function App() {
   const [showWelcomeScreen, setShowWelcomeScreen] = useState(true);
@@ -34,6 +35,7 @@ function App() {
         </div>
       ) : (
         <>
+          <Header/>
           <Content />
           <Navbar />
         </>
